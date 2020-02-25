6c531852da78752062db397c5e84bb128d4f3ef8
iratxeMoya <iratxe.moya@gmail.com>
2020-02-25 19:39:10 +0100

    me muero

---
+++ src/js/client.js
@@ -0,0 +1,27 @@
+
+import { connection } from './init';
+
+connection.onopen = event => {
+	console.log('connection is open');
+}
+
+connection.onclose = (event) => {
+    console.log("WebSocket is closed");
+};
+
+connection.onerror = (event) => {
+    console.error("WebSocket error observed:", event);
+};
+
+connection.onmessage = (event) => {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'createSeminar') {
+        var parent = document.querySelector("postContainer");
+        var seminar = document.createElement("span");
+
+        seminar.innerText = data.subject;
+
+        parent.appendChild(seminar);
+    }
+}
