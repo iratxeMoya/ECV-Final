8a70e893a2d9a53eba7d351935e7756ffeae97b9
iratxeMoya <iratxe.moya@gmail.com>
2020-02-26 14:40:29 +0100

    module creado masomenos (fijo que esta mal pero meh

--- src/js/module.js
+++ src/js/module.js
@@ -10,22 +10,65 @@ class Module {
      * @param {Int} id Unique ??????? no se como hacer esto
      */
     constructor (position, code, target, id) {
-        this.previous = previous;
-        this.next = next;
         this.position = position;
         this.code = code;
         this.target = target;
         this.id = id;
+        this.before = null;
+        this.after = null;
     }
-
+/**
+ * 
+ * @param {Object} newPosition {x, y}
+ * 
+ * Moves the Module to a new position in the Canvas
+ */
     move (newPosition) {
         this.position = newPosition;
         // aqui hacer lo que haya que hacer para moverlo en el canvas ????
 
         var jsonData = {};
-        jsonData.moduleId = id;
+        jsonData.type = 'moveModule'
+        jsonData.moduleId = this.id;
         jsonData.newPosition = newPosition;
 
         connection.send(JSON.stringify(jsonData));
+    }
+    /**
+     * Deletes the Module
+     */
+    delete () {
+        //! NO SE COMO HACER ESTO
+    }
+    /**
+     * 
+     * @param {Module} module 
+     * @param {String} position before / after
+     */
+    relate(module, position) {
+
+        if (position === 'before') {
+            this.before = module;
+        } else {
+            this.after = module;
+        }
+
+        var jsonData = {};
+        jsonData.type = 'relateModules';
+        jsonData.moduleId = this.id;
+        jsonData.position = this.position;
+        jsonData.code = this.code;
+        jsonData.target = this.target;
+        jsonData.before = this.before;
+        jsonData.after = this.after;
+        
+        connection.send(JSON.stringify(jsonData));
+    }
+
+    executeCode () {
+        eval(this.code);
+
+        //! esto entiendo que no hay que pasarselo al server ya que cada cliente ejecuta el codigo
+        //! cuando le da la gana no?
     }
 }
