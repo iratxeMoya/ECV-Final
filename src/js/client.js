5a7edd546fdcc1443f22864ee585aa03fcdd05c6
Pau Pamos Gil <paupamos@.com>
2020-02-26 18:44:38 +0100

    iratxetienehambre

--- src/js/client.js
+++ src/js/client.js
@@ -12,8 +12,16 @@ var module_manager = new ModuleManager();
 var test_module1 = new ArgModule({x:100,y:100}, "log", "none" , 0,"HI");
 var test_module2 = new ArgModule({x:200,y:200}, "log", "none" , 0,"HO")
 
+var workbench = document.getElementsByClassName("user_screen")[0];
+
+wb_h =workbench.style.height;
+wb_w =workbench.style.width;
+
 module_manager.add_module(test_module1);
 module_manager.add_module(test_module2);
+
+cvs.height=wb_h;
+cvs.width=wb_w;
 
 var mouseX;
 var mouseY;
@@ -21,6 +29,8 @@ var mouseY;
 var moving = [];
 
 cvs.addEventListener("mousemove",function(event){
+		mouseX=event.clientX;
+		mouseY=event.clientY;
 		module_manager.move_modules(event.clientX,event.clientY);
 	});
 cvs.addEventListener("mousedown",function(event){
@@ -32,7 +42,10 @@ cvs.addEventListener("mouseup",function(event){
 
 
 function update(){
-	
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0, 0, wb_w, wb_h);
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(mouseX, mouseY, 5, 5);
 	module_manager.draw(ctx);
 }
 
