e69c394134da0f7b3e63fec91415d5152e035138
Iratxe Moya <imoya@BCNSDWW19IMOYA.scytl.net>
2020-02-28 10:11:25 +0100

    delete?

--- server.js
+++ server.js
@@ -80,15 +80,14 @@ wss.on('connection', function(ws) {
                     return
                 }
 
-                var jsonData = JSON.parse(jsonString);
-                console.log(info);
+                var json = JSON.parse(jsonString);
 
-                fs.writeFile("src/data/modules.json", jsonContent, 'utf8', function (err) {
+                fs.writeFile("src/data/modules.json", json, 'utf8', function (err) {
                     if (err) {
                         return console.log(err);
                     }
                 
-                    console.log("The file was saved!");
+                    console.log("The file was saved! ", json);
                 });
             })
 
