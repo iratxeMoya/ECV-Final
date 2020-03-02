6f028810a1f6ed7139d6370534fa95277e724627
Pau Pamos Gil <paupamos@.com>
2020-03-02 20:43:43 +0100

    ASDç

--- src/js/module.js
+++ src/js/module.js
@@ -163,6 +163,8 @@ class Module {
 	}
 	
 	getTarget(){
+		
+		console.log('in get target ', this, typeof this)
 		return this.prev ? this.prev.getTarget() : (typeof this === "TargetModule" ? this.target : null);
 	}
 
