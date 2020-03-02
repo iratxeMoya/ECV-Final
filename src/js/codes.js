5e364c56a1d4586d23db166b4bf166b52947ad06
Pau Pamos Gil <paupamos@.com>
2020-03-02 20:40:10 +0100

    ASDç

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,4 +1,4 @@
-var codes ={"log1":"console.log('$arg$');","log2":"console.log(this.getTarget();"}
+var codes ={"log1":"console.log('$arg$');","log2":"console.log(this.getTarget());"}
 
 var styles = {"log1":"#0000FF","log2":"#FF0000"}
 
