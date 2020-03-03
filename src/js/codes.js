ba620a3e7916c46b819c6cb1279d5f940e845910
u137434 <u137434@54030p10u.ds.upf.edu>
2020-03-03 16:35:32 +0100

    holis

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,4 +1,4 @@
-var codes ={"log1":"console.log('$arg$');","log2":"console.log('x: ', this.getTarget().parameters.posx, ' y: ', this.getTarget().parameters.posy);"}
+var codes ={"log1":"console.log('$arg$');","log2":"console.log('x: ', this.getTarget().parameters, ' y: ', this.getTarget().parameters);"}
 
 var styles = {"log1":"#0000FF","log2":"#FF0000", 'target': '#00FF00'}
 
