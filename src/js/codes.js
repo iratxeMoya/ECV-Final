1c44fca65b434f6cc21a53f012b31054533aa285
u137434 <u137434@54030p10u.ds.upf.edu>
2020-03-03 15:52:37 +0100

    holis

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,6 +1,6 @@
 var codes ={"log1":"console.log('$arg$');","log2":"console.log('x: ', this.getTarget().parameters.posx, ' y: ', this.getTarget().parameters.posy);"}
 
-var styles = {"log1":"#0000FF","log2":"#FF0000", undefined: '#00FF00'}
+var styles = {"log1":"#0000FF","log2":"#FF0000", 'target': '#00FF00'}
 
 export{
 	codes,
