63d05c98860d797a41554c89175c5602e90a316e
u137434 <u137434@54005p04u.ds.upf.edu>
2020-03-03 18:51:26 +0100

    bajkshd

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,4 +1,4 @@
-var codes ={"log1":"console.log('$arg$');","log2":"console.log('x: ', this.getTarget()[0].parameters.posx, ' y: ', this.getTarget()[0].parameters.posy);"}
+var codes ={"log1":"console.log('$arg$');","log2":"this.getTarget()[0].parameters.posx = this.getTarget()[0].parameters.posx+10;"}
 
 var styles = {"log1":"#0000FF","log2":"#FF0000", 'target': '#00FF00'}
 
