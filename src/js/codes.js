dcb7207a3f67f8667e2e4a81bb6fe069ea4ce60f
Pau Pamos Gil <paupamos@.com>
2020-03-05 20:38:12 +0100

    asd

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,4 +1,4 @@
-var codes ={"ifwall":"target.parameters.posx>200","log":"console.log('$arg$');","move":"target.parameters.posx = target.parameters.posx+10;"}
+var codes ={"ifwall":"target.parameters.posx>200","log":"console.log('$arg$');","move":"console.log(target);target.parameters.posx = target.parameters.posx+10;"}
 
 var styles = {"log":"#0000FF","ifwall":"#999999","move":"#FF0000", 'target': '#00FF00'}
 
