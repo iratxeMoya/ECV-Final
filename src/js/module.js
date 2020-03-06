8353239365292aba98adc019d6f6988720ef29fa
Iratxe Moya <imoya@BCNSDWW19IMOYA.scytl.net>
2020-03-06 12:42:41 +0100

    ahora todo es mas divino

--- src/js/module.js
+++ src/js/module.js
@@ -16,7 +16,7 @@ class Element {
     }
 	
 	draw(gs_ctx){
-		gs_ctx.fillStyle = '#00FF00';
+		gs_ctx.fillStyle = '#FF6DC9';
         gs_ctx.fillRect(this.parameters.posx-this.parameters.sizex/2,this.parameters.posy-this.parameters.sizey/2, this.parameters.sizex,this.parameters.sizey);
 	}
 	
