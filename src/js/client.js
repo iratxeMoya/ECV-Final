d0d5ba878f84b6e6300d2bf4e9fdccd9b58b91f3
iratxeMoya <iratxe.moya@gmail.com>
2020-03-07 19:12:05 +0100

    hola pau que tal

--- src/js/client.js
+++ src/js/client.js
@@ -123,7 +123,7 @@ function release(event) {
 	}
 
 
-	connection.send(JSON.stringify({type: 'releaseModule', posx: event.offsetX, posy: event.offsetY, remove: remove, modules: modules, screenX: screenX, screenY: screenY}));
+	connection.send(JSON.stringify({type: 'releaseModule', posx: event.offsetX, posy: event.offsetY, remove: remove, modules: modules, screenX: event.screenX, screenY: event.screenY}));
 
 }
 
