de29d8149ea66e0c4c9a2b989687fb96a6c1ad5c
iratxeMoya <iratxe.moya@gmail.com>
2020-03-07 18:00:26 +0100

    hola pau que tal

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ wss.on('connection', function(ws) {
         
         if (jsonData.type === 'login') {
             
-            //var foundUser = registeredUsers.find(user => user.username === jsonData.username);
+            var foundUser = registeredUsers.find(user => user.username === jsonData.username);
 
             if (foundUser && passwordHash.verify(jsonData.password, foundUser.hashedPassword)) {
                 var sendData = {};
@@ -77,8 +77,8 @@ wss.on('connection', function(ws) {
         }
         else if (jsonData.type === 'register') {
 
-            //var foundClient = registeredUsers.find(user => user.username === jsonData.username);
-			if (!false) { //en si es foundClient
+            var foundClient = registeredUsers.find(user => user.username === jsonData.username);
+			if (!foundClient) {
 
                 var sendData = {};
                 sendData.type = 'connectionResponse';
@@ -233,6 +233,7 @@ function loadDatabaseFromDisk()
 
     var str2 = fs.readFileSync('src/data/users.json').toString();
     registeredUsers = jsonToArray(JSON.parse( str2 ));
+    console.log('in load db: ', registeredUsers)
     
 }
 
