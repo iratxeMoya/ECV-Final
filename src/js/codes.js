4242ee52489e7e17d7ee446324fa6aba1660e33f
Pau Pamos <pau.pamos01@estudiant.upf.edu>
2020-03-07 16:17:44 +0100

    ASD

--- src/js/codes.js
+++ src/js/codes.js
@@ -1,6 +1,6 @@
-var codes ={"ifwall":"!(target.in_range(target.next_pos(),100,200,100,200))","log":"console.log('$arg$');","move":"target.forward();","turn":"target.turn_clock();"}
+var codes ={"ifwall":"!(target.in_range(target.next_pos(),100,400,100,400))","log":"console.log('$arg$');","move":"target.forward();","turn":"target.turn_clock();"}
 
-var styles = {"log":"#0000FF","ifwall":"#999999","move":"#FF0000", 'target': '#00FF00'}
+var styles = {"log":"#0000FF","ifwall":"#999999","move":"#FF0000", 'target': '#00FF00','turn':'#0000FF'}
 
 export{
 	codes,
