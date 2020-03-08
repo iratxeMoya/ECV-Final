948f1c756e7d68805d2bd3ec86449fdbbfdee4cc
Pau Pamos <pau.pamos01@estudiant.upf.edu>
2020-03-08 12:49:36 +0100

    asd

--- src/js/module.js
+++ src/js/module.js
@@ -635,7 +635,7 @@ class ModuleManager {
 	run_modules() {
 		if (this.running){
 			this.modules.forEach(module => {
-			
+				console.log("MODULE\n"+module)
 				module.type === "target" ? (!module.target.dead ? module.run():null) : null;
 
 			});
