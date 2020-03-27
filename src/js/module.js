36ce53544b62e59dd6f310b65306512e1a94166d
Pau Pamos <pau.pamos01@estudiant.upf.edu>
2020-03-27 15:14:11 +0100

    asd

--- src/js/module.js
+++ src/js/module.js
@@ -122,7 +122,7 @@ class Element {
 	}
 	
 	draw(gs_ctx) {
-
+		ctx.globalCompositeOperation = "hue";
 		gs_ctx.drawImage(element_img, 0, 0,50,50,(this.position.x - 1) * ELEMENTSIZE,(this.position.y - 1) * ELEMENTSIZE,ELEMENTSIZE,ELEMENTSIZE);
 		gs_ctx.fillStyle = '#FF6DC9';
 		gs_ctx.fillRect((this.position.x - 1) * ELEMENTSIZE, (this.position.y - 1) * ELEMENTSIZE, ELEMENTSIZE, ELEMENTSIZE);
