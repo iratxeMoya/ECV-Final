d352edb6bbdfd6c6a24f5e92c8063eabe3621751
iratxeMoya <iratxe.moya@gmail.com>
2020-03-27 19:08:21 +0100

    asjvd

--- src/js/codes.js
+++ src/js/codes.js
@@ -4,19 +4,19 @@ var codes = {
 
 		"rndir":"target.dir=Math.floor(Math.random()*100)%4;",
 		"move":"target.forward();",
-		"turn":"target.turn_clock();",
+		"turn_clock":"target.turn_clock();",
+		"turn_counter":"target.turn_counter();",
 
 	},
 	"condition": {
 
 		"ifwall":"target.colision(this.map);",
-		"facenop":"target.dir==this.map.face_nop(target.position.x,target.position.y)",
-		"nopdistless":"this.map.dist_nop(target.position.x,target.position.y)<$val$"
+		"face_oponent":"target.dir==this.map.face_nop(target.position.x,target.position.y)",
+		"oponent_near":"this.map.dist_nop(target.position.x,target.position.y)<5"
 
 	},
 	"control": {
 
-		"log":"console.log('$arg$');",
 		"empty":""
 
 	}
@@ -26,7 +26,7 @@ var styles = {
 	"control":"#0000FF",
 	"condition":"#999999",
 	"movement":"#FF0000",
-	"target":"#AAFFAA"
+	"target":"#88CC88"
 	}
 
 
